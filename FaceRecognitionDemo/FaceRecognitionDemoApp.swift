28b415a4b3bef02bd5c95cc75c7fe30147faa8b0
Abelito Faleyrio Visese <afvisese@gmail.com>
2025-06-02 10:55:46 +0700

    Initial Commit

---
+++ FaceRecognitionDemo/FaceRecognitionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceRecognitionDemoApp.swift
+//  FaceRecognitionDemo
+//
+//  Created by Abelito Faleyrio Visese on 02/06/25.
+//
+
+import SwiftUI
+
+@main
+struct FaceRecognitionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FaceRecognitionDemoTests/FaceRecognitionDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  FaceRecognitionDemoTests.swift
+//  FaceRecognitionDemoTests
+//
+//  Created by Abelito Faleyrio Visese on 02/06/25.
+//
+
+import Testing
+@testable import FaceRecognitionDemo
+
+struct FaceRecognitionDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
